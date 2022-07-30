60f040bdf848f92804ea831434fdfd6093c37eac
Corbyn Altman <corbyn.altman01@gmail.com>
2022-07-30 19:37:07 -0400

    style changes

--- src/components/GameHeader.jsx
+++ src/components/GameHeader.jsx
@@ -1,7 +1,7 @@
 const GameHeader = () => {
   return (
     <div className="game-header">
-      <h1>Tenzies</h1>
+      <h1 className="grad-text">Tenzies</h1>
       <p>
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
